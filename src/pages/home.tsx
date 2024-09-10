4861cd4c6f9732b1009158e5e60b1cfc0c44e417
BrunaBallerini <bruna@webgoal.com.br>
2024-09-10 19:01:07 -0300

    Fazer requisicao do token armazenado

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,14 +2,44 @@ import ImgHome from '../assets/images/ImgHome.png'
 import LogoImage from '../assets/images/Logo.svg'
 import ButtonAccept from '../components/inputs/buttonaccept'
 import '../style/home.css'
+import { useEffect, useState } from 'react';
+import api from '../server/api';
 
 function Home() {
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    api.get('/login')
+      .then(response => {
+        console.log(response);
+        console.log(response.data);
+
+        setUserName(response.data.email);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  const token = localStorage.getItem('jwt'); // Obtém o token armazenado
+
+  fetch('http://localhost:3000/login', {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`, // Adiciona o token no header
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Erro ao buscar dados', error));
+
 
   return (
     <div className='home-content'>
       <div className='home-head'>
         <img className="logo" src={LogoImage} alt="Logo do Secrets Friends" />
-        <h2>Olá Pessoa</h2>
+        <h2>Olá {userName}</h2>
       </div>
       <div className='img-text'>
         <img src={ImgHome} alt="Pessoa feliz por ter logado." />
