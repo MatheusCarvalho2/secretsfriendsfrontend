2ba6fec06ec2c75ff9121937157f7e0cb25b3c35
BrunaBallerini <bruna@webgoal.com.br>
2024-10-10 16:34:40 -0300

    Retirarndo inicialização da store no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,19 @@ import Home from "./pages/home";
 import Create from "./pages/create";
 import ParticipantsList from "./pages/participants_list";
 import AddParticipant from "./pages/add_participantes";
-import { Provider } from "react-redux";
-import store from "./store";
-
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/cadastro" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/criar" element={<Create />} />
-          <Route path="/lista" element={<ParticipantsList />} />
-          <Route path="/adicionar" element={<AddParticipant />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/criar" element={<Create />} />
+        <Route path="/lista" element={<ParticipantsList />} />
+        <Route path="/adicionar" element={<AddParticipant />} />
+      </Routes>
+    </Router>
   );
 };
 
