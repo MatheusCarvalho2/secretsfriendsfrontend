27f257177b9f5e07fdff246baa736297c053e2ca
BrunaBallerini <bruna@webgoal.com.br>
2024-10-21 11:09:23 -0300

    Retirada de passwordConfirmation para profileSignIn

--- src/store/profile/actions.ts
+++ src/store/profile/actions.ts
@@ -4,17 +4,18 @@ import { ProfileTypes } from './types';
 interface ProfileProps {
   email: string,
   password: string,
-  passwordConfirmation: string,
 }
 
 export const profileSignOut = () => ({
+  type: ProfileTypes.SHOW_REQUEST,
   types: [ProfileTypes.SHOW_REQUEST, ProfileTypes.SHOW_SUCCESS, ProfileTypes.SHOW_FAILURE],
-  promise: (user: any) => user.delete('http://localhost:3000/logout'),
+  promise: (client: any) => client.delete('http://localhost:3000/logout'),
 });
 
-export const profileSignIn = ({ email, password, passwordConfirmation }: ProfileProps) => ({
+export const profileSignIn = ({ email, password }: ProfileProps) => ({
+  type: ProfileTypes.SHOW_REQUEST,
   types: [ProfileTypes.SHOW_REQUEST, ProfileTypes.SHOW_SUCCESS, ProfileTypes.SHOW_FAILURE],
-  promise: (user: any) => user.post('http://localhost:3000/signup', { user: { email, password, passwordConfirmation }, }),
+  promise: (client: any) => client.post('http://localhost:3000/login', { user: { email, password }, }),
 });
 
 export default ProfileProps;
