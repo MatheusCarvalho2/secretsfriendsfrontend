d51f72f60eba03266e6e3ef72aa8a3b43789927a
BrunaBallerini <bruna@webgoal.com.br>
2024-10-24 20:48:37 -0300

    Criação da index.ts da nova store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import nameSlice from './reducers/name.ts';
+import emailSlice from './reducers/email.ts';
+import isSigned from './reducers/isSigned.ts';
+import tolkenSlice from './reducers/tolken.ts';
+import presentListSlice from './reducers/presentsList.ts';
+import secretFriendSlice from './reducers/secretFriend.ts';
 
-import rootReducer from './rootReducer'
-
-const store = configureStore({ reducer: rootReducer })
-
+const store = configureStore({
+    reducer: {
+        name: nameSlice,
+        email: emailSlice,
+        isSigned: isSigned,
+        tolken: tolkenSlice,
+        presents_list: presentListSlice,
+        secret_friend: secretFriendSlice,
+    }
+});
 export default store;
