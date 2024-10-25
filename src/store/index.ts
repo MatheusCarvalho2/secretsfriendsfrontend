2ac31ad1894cf119981bd9d924239ce2c2fff23e
BrunaBallerini <bruna@webgoal.com.br>
2024-10-25 18:51:38 -0300

    Configura a biblioteca redux-persist para salvar dados na store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
 import nameSlice from './reducers/name.ts';
 import emailSlice from './reducers/email.ts';
-import isSigned from './reducers/isSigned.ts';
+import isSignedSlice from './reducers/isSigned.ts';
 import tokenSlice from './reducers/token.ts';
 import presentListSlice from './reducers/presentsList.ts';
 import secretFriendSlice from './reducers/secretFriend.ts';
+import storage from 'redux-persist/es/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const rootReducer = combineReducers({
+    name: nameSlice,
+    email: emailSlice,
+    isSigned: isSignedSlice,
+    token: tokenSlice,
+    presents_list: presentListSlice,
+    secret_friend: secretFriendSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-    reducer: {
-        name: nameSlice,
-        email: emailSlice,
-        isSigned: isSigned,
-        tolken: tokenSlice,
-        presents_list: presentListSlice,
-        secret_friend: secretFriendSlice,
-    }
+    reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
 export default store;
