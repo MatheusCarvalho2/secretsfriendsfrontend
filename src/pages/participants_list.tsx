b91eeda4e37435bc854b7f33781eefd29c20de0d
BrunaBallerini <bruna@webgoal.com.br>
2024-11-04 14:08:17 -0300

    Cria página /lista mostrando os participantes do sorteio

--- src/pages/participants_list.tsx
+++ src/pages/participants_list.tsx
@@ -6,8 +6,7 @@ import ButtonAccept from '../components/inputs/buttonaccept';
 import status2Breadcrumb from '../assets/images/status-2.png'
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
-
-// import api from '../server/api';
+import api from '../server/api';
 
 function ParticipantsList() {
 
@@ -16,28 +15,35 @@ function ParticipantsList() {
   }
 
   const participantsListStore = useSelector((state: RootState) => {
-    console.log(state.participant_list);
-    return state.participant_list;
+    return state.participantList;
   });
 
-  // const handleDrawUser = (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
+  const currentDrawId = useSelector((state: RootState) => {
+    return state.idDraw;
+  });
 
-  //   api.post('/draw_users',
-  //     {
-  //       "owner": true,
-  //       "suggestion": '',
-  //       "user_id": "user.id", //store
-  //       "draw_id": "draw.id", //store
-  //     },
-  //   )
-  //     .then(response => {
-  //       console.log(response.data);
-  //     })
-  //     .catch(error => {
-  //       console.error("Erro:", error);
-  //     });
-  // };
+  const currentUserId = useSelector((state: RootState) => {
+    return state.idUser;
+  })
+
+  const handleFinish = () => {
+    api.post('/draw_users',
+      {
+        "owner": true,
+        "suggestion": '',
+        "user_id": currentUserId,
+        "draw_id": currentDrawId,
+      },
+    )
+      .then(response => {
+        if (response.status >= 200 && response.status <= 299) {
+          window.location.href = '/sorteio_realizado'
+        }
+      })
+      .catch(error => {
+        console.error("Erro:", error);
+      });
+  };
 
   return (
     <>
@@ -54,13 +60,24 @@ function ParticipantsList() {
           </div>
           <img src={status2Breadcrumb} alt="Etapa um de três" className='steps' />
         </div>
-        <div className='mensagem'>
-          {participantsListStore.length > 0 ? <h3>Tem coisa</h3> : <h3>Você não tem nenhum partipante cadastrado</h3>}
+        <div className='message'>
+          {participantsListStore.length > 0 ?
+            participantsListStore.map((participant, index) => (
+              <div className='message-participant' key={index}>
+                <h3>{index} - {participant}</h3>
+              </div>
+            )) : <h3>Você não tem nenhum partipante cadastrado</h3>}
         </div>
-        <ButtonAccept
-          textButton="Adicionar"
-          onClick={handleModal}
-        />
+        <div className='buttons-list'>
+          <ButtonAccept
+            textButton="Adicionar"
+            onClick={handleModal}
+          />
+          <ButtonAccept
+            textButton="Finalizar"
+            onClick={handleFinish}
+          />
+        </div>
       </div>
     </>
   )
