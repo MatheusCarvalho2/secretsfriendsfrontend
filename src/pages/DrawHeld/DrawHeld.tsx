13c419fce328ee4be9b98665ca1ec9f837c07671
BrunaBallerini <bruna@webgoal.com.br>
2024-11-19 16:47:58 -0300

    Implementação do uso da biblioteca emailjs para envio de emails quando sorteio realizado

--- src/pages/DrawHeld/DrawHeld.tsx
+++ src/pages/DrawHeld/DrawHeld.tsx
@@ -1,9 +1,39 @@
+import { RootState } from '../../store';
 import ImgDrawHeld from '../../assets/images/DrawHeld.png'
 import LogoImage from '../../assets/images/Logo.svg'
 import ButtonAccept from '../../components/ButtonAccept/ButtonAccept'
 import api from '../../server/api';
+import emailjs from 'emailjs-com';
+import { useSelector } from 'react-redux';
 
 function DrawHeld() {
+    const participantsList = useSelector((state: RootState) => state.participantList);
+    const secretFriend = useSelector((state: RootState) => state.secretFriend);
+
+
+    function sendEmails() {
+        participantsList.forEach((participant: string) => {
+            const templateParams = {
+                replyTo: participant,
+                secretFriend: secretFriend,
+            };
+
+            emailjs
+                .send(
+                    'service_n5qrsko',
+                    'template_x3rqkit',
+                    templateParams,
+                    'R0ygy8ZywRVBfv0YF'
+                )
+                .then(() => {
+                    console.log('E-mail enviado com sucesso!');
+                })
+                .catch((error: string) => {
+                    console.error('Erro ao enviar o e-mail:', error);
+                });
+        });
+    }
+
     const logout = () => {
         api.delete('/logout')
             .then(response => {
@@ -26,6 +56,10 @@ function DrawHeld() {
                 <img src={ImgDrawHeld} alt="Caixa de presente aberta saindo confetes de dentro." />
             </div>
             <div className='buttons-home'>
+                <ButtonAccept
+                    textButton='Sorteios'
+                    onClick={sendEmails}
+                />
                 <ButtonAccept
                     textButton='Logout'
                     onClick={logout}
