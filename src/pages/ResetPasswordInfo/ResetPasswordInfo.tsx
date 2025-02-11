79a24c965ff2fdbbccb7b85ffd7b564f296703d7
BrunaBallerini <bruna@webgoal.com.br>
2025-02-11 17:45:59 -0300

    Melhora a página de informação de email enviado

--- src/pages/ResetPasswordInfo/ResetPasswordInfo.tsx
+++ src/pages/ResetPasswordInfo/ResetPasswordInfo.tsx
@@ -1,17 +1,44 @@
+import { useSelector } from "react-redux";
+import { RootState } from '../../store';
+import ButtonAccept from "../../components/ButtonAccept/ButtonAccept";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
+import './ResetPasswordInfo.css'
+
 const ResetPasswordInfo = () => {
+
+    const emailResetPassword = useSelector((state: RootState) => {
+        return state.emailResetPassword;
+    });
+
     return (
 
-        <div className="message">
-            <h3>
-                Solicitação enviada com sucesso!
-            </h3>
-            <p>
-                Verifique sua caixa de entrada do email hugo@webgoal
-            </p>
-        </div>
+        <div className='home-content'>
+            <div className='info-header'>
+                <Link to="/" className='back-arrow'>
+                    <FaArrowLeft size={20} />
+                </Link>
+                <Link to="/" className='back-arrow'>
+                    <FaXmark size={20} />
+                </Link>
+            </div>
+            <div className="message-info">
+                <h3 className="sucess">
+                    Solicitação enviada com sucesso!
+                </h3>
+                <p>
+                    Verifique sua caixa de entrada do email {emailResetPassword}.
+                </p>
+            </div>
 
-
+            <div className='buttons-list'>
+                <ButtonAccept
+                    textButton="Voltar"
+                    onClick={() => { window.location.href = '/' }}
+                />
+            </div>
+        </div >
     )
 }
-
 export default ResetPasswordInfo
