ed7526fb6055aed5d2f6af62b8712679a206d2a6
BrunaBallerini <bruna@webgoal.com.br>
2025-02-11 17:28:32 -0300

    Melhora formatação dentro da página home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,11 +15,11 @@ function Home() {
     <div className='home-content'>
       <div className='home-head'>
         <img className="logo" src={LogoImage} alt="Logo do Secrets Friends" />
-        <h2>Olá {currentName ? currentName : 'Amigo'}</h2>
+        <h2>Olá, {currentName ? currentName : 'Amigo'}</h2>
       </div>
       <div className='img-text'>
         <img src={ImgHome} alt="Caixa de presente brana com uma fita azul." />
-        <h3 className='message-home'>Crie o seu primeiro sorteio agora!</h3>
+        <h3 className='message-home'>Crie o seu primeiro<br /> sorteio agora!</h3>
       </div>
       <div className='buttons-home'>
         <ButtonAccept
