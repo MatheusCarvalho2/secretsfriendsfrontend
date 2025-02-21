1734173f18523670783fcbe7a41cc38002ea66e8
BrunaBallerini <bruna@webgoal.com.br>
2025-02-21 16:43:09 -0300

    Retira rota de addParticipant

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Register from "./pages/Register/Register";
 import Home from "./pages/Home/Home";
 import Create from "./pages/Create/Create";
 import ParticipantsList from "./pages/ParticipantsList/ParticipantsList";
-import AddParticipant from "./pages/AddParticipant/AddParticipants";
 import DrawHeld from "./pages/DrawHeld/DrawHeld";
 import DrawSent from "./pages/DrawSent/DrawSent";
 import DrawSentConfirmation from "./pages/DrawSentConfirmation/DrawSentConfirmation";
@@ -22,7 +21,6 @@ const App = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/criar" element={<Create />} />
         <Route path="/lista" element={<ParticipantsList />} />
-        <Route path="/adicionar" element={<AddParticipant />} />
         <Route path="/sorteio_realizado" element={<DrawHeld />} />
         <Route path="/envia_email" element={<DrawSent />} />
         <Route path="/emails_enviados" element={<DrawSentConfirmation />} />
